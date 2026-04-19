c234849ed2da1b0fcc474ca7c91ccaac3e3ee402
Eunji1120 <eunji1120@outlook.com>
2026-04-19 01:12:53 -0700

    same

--- base_model.r
+++ base_model.r
@@ -27,21 +27,3 @@ fit1$cmdstan_diagnose()
 # trace plot
 library(bayesplot)
 mcmc_trace(fit1$draws(c("mu", "sigma", "beta[7]", "beta[8]")))
-
-#studentt_model
-mod2 <- cmdstan_model("studentt_model.stan")
-
-fit2 <- mod2$sample(
-  data    = stan_data,
-  seed    = 123,
-  chains  = 4,
-  iter_warmup   = 1000,
-  iter_sampling = 2000,
-  refresh = 500
-)
-
-fit2$summary()
-fit2$cmdstan_diagnose()
-
-mcmc_trace(fit2$draws(c("mu", "sigma", "nu", "beta[7]", "beta[8]")))
-
